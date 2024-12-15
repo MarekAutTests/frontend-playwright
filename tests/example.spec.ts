f39c58a92b70ea60d5eb30bb28d069d42d84cd34
Marek Vorp <marek.vorp@gmail.com>
2024-12-15 23:40:38 +0200

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,26 @@
+import {test, expect} from '@playwright/test'
+import {faker} from '@faker-js/faker'
+import {configDotenv} from "dotenv"
+
+configDotenv()
+
+test.beforeEach(async ({page}) => {
+  await page.goto(process.env.URL)
+})
+
+test('Check submit enabled', async ({page}) => {
+  const submitButton = page.getByTestId('signIn-button')
+  await expect(submitButton).toBeEnabled()
+})
+
+test('Check PopUp visible', async ({page}) => {
+  const userNameField = page.getByTestId('username-input')
+  const userEmailField = page.getByTestId('password-input')
+
+  await userNameField.fill("test")
+  await userEmailField.fill("test@test.com")
+  const submitButton = await page.getByTestId('signIn-button')
+  await submitButton.click()
+  const authPopUp = page.getByTestId('authorizationError-popup')
+  await expect(authPopUp).toBeVisible()
+})
